c90df2f3300e1e6dce75644a85f98a37eb4082aa
Christian Josue Gonzalez Lozano <cjgonzalez@entradagroup.com>
2026-07-28 08:13:42 -0600

    fix: update wildcard route pattern for Express 5 compatibility

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // SPA redirection to index.html for all other routes
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
